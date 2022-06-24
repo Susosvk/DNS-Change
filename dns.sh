b41cc82d13b387cfc97c4234fd18197225df3116
Susosvk <suso@k3s4.sk>
2022-06-24 11:29:46 +0200

    Update dns.sh
    Pridanie Cat a echo

--- dns.sh
+++ dns.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 cp /etc/resolv.conf /etc/resolv.conf.bak && printf "nameserver 10.224.128.6\nnameserver 10.224.128.7\nsearch dedoles.net" > /etc/resolv.conf
+
+cat /etc/resolv.conf
+echo.
+echo.
